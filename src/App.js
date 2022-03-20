3e0f641a5c3cf43bee99141cae99b9b6185893a7
Praveen Dias <praveendias1180@gmail.com>
2022-03-20 21:09:53 +0530

    added signup route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/SignIn';
+import SignUp from './components/SignUp';
 import Home from './components/Home';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
