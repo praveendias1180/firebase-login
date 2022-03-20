5d4a5dc1113065c4a29c93bcc561580c0070ecdb
Praveen Dias <praveendias1180@gmail.com>
2022-03-20 14:04:52 +0530

    index.js added to version control

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import firebaseConfig from '../firebase.config';
+import { 
+    getAuth, 
+    onAuthStateChanged,
+    connectAuthEmulator,
+    signInWithEmailAndPassword,
+} from 'firebase/auth'
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 const auth = getAuth(firebaseApp)
+
+connectAuthEmulator(auth, 'http://localhost:9099')
+
+const loginEmailPassword = async () => {
+
+    // No UI yet.
+    // const loginEmail = txtEmail.value
+    // const loginPassword = txtPassword.value
+
+    const loginEmail = 'hello@world.com' 
+    const loginPassword = 'ksi$di4RSdsiodfuu#dfs#' 
+
+    const userCredentials = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+    console.log(userCredentials);
+}
+
+// No UI yet.
+// btnLogin.addEventListener('click', loginEmailPassword)
+
 const db = getFirestore(firebaseApp)
 
 // Detec auth state
@@ -17,3 +41,6 @@ onAuthStateChanged(auth, user => {
         console.log('No user');
     }
 })
+
+// No UI yet.
+loginEmailPassword()
