2f0b3e94a9108bd8abe00fa4bd3fdc28086c67cf
Praveen Dias <praveendias1180@gmail.com>
2022-03-20 15:55:39 +0530

    Moved all sign in logic to sign in page

--- src/index.js
+++ src/index.js
@@ -1,46 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-import firebaseConfig from '../firebase.config';
-import { 
-    getAuth, 
-    onAuthStateChanged,
-    connectAuthEmulator,
-    signInWithEmailAndPassword,
-} from 'firebase/auth'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-const auth = getAuth(firebaseApp)
-
-connectAuthEmulator(auth, 'http://localhost:9099')
-
-const loginEmailPassword = async () => {
-
-    // No UI yet.
-    // const loginEmail = txtEmail.value
-    // const loginPassword = txtPassword.value
-
-    const loginEmail = 'hello@world.com' 
-    const loginPassword = 'ksi$di4RSdsiodfuu#dfs#' 
-
-    const userCredentials = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-    console.log(userCredentials);
-}
-
-// No UI yet.
-// btnLogin.addEventListener('click', loginEmailPassword)
-
-const db = getFirestore(firebaseApp)
-
-// Detec auth state
-onAuthStateChanged(auth, user => {
-    if(user != null){
-        console.log('logged in!');
-    } else {
-        console.log('No user');
-    }
-})
-
-// No UI yet.
-loginEmailPassword()
+ReactDOM.render(
+  <App />,
+  document.getElementById('range_root')
+);
