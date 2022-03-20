6e9b65ff643813a15ab0c5e116914fa847247e13
Praveen Dias <praveendias1180@gmail.com>
2022-03-20 15:20:05 +0530

    React and Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    watch: true,
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
-}
+  mode: 'development',
+  devtool: 'eval-source-map',
+  watch: true,
+  entry: ["babel-polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+            presets: ['@babel/env','@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
