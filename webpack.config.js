add08276ce88cc23c4e9f0ee4bd2c53dac94e89c
Praveen Dias <praveendias1180@gmail.com>
2022-03-20 14:00:55 +0530

    webpack is now watching! 💲

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
+    watch: true,
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
